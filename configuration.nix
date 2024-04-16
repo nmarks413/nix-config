112e90204b3b01bd01694998cb7e38a906d88f2c
Natalie Marks <nmarks413@gmail.com>
2024-04-16 00:32:54 -0700

    updated system config

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,7 @@
 
   systemd.services.duckdns = {
     enable = true;
-    script = ''echo url="https://www.duckdns.org/update?domains=pathfinder2e&token=9c1ffa47-7496-4975-ba2b-a6928b28c500&ip=" | ${pkgs.nix}/bin/curl -k -o ~/.duckdns/duck.log -K -'';
+    script = ''echo url="https://www.duckdns.org/update?domains=pathfinder2e&token=9c1ffa47-7496-4975-ba2b-a6928b28c500&ip=" | ${pkgs.curl}/bin/curl -k -o ~/.duckdns/duck.log -K -'';
     serviceConfig = {
       Type = "oneshot";
       User = "root";
