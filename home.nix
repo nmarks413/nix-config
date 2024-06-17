9919b096a78e75285b5a05ed55cd8d58d99c0a78
Natalie Marks <nmarks413@gmail.com>
2024-06-17 01:26:46 -0700

    updated home-manager config

--- home.nix
+++ home.nix
@@ -40,7 +40,9 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
+
   home.packages = with pkgs; [
+    zigpkgs."0.13.0"
     stylua
     webcord
     btop
