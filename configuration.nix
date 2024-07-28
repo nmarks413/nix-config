5f015c4afd8457982473773ea3c49031d17412cc
Natalie Marks <nmarks413@gmail.com>
2024-07-28 01:35:02 -0700

    updated system config

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,14 @@
 }: {
   nixpkgs.config.allowUnfree = true;
 
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = ["nmarks"];
+  };
+
   services.blueman.enable = true;
   hardware.bluetooth.enable = true;
 
