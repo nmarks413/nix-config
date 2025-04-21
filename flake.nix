c2d01f86b0cda4d50ba4921c086425d1dd96282b
Natalie <nmarks413@gmail.com>
2025-04-21 13:04:55 -0700

    stylix working 2

--- flake.nix
+++ flake.nix
@@ -78,7 +78,7 @@
       name = "Natalie"; # name/identifier
       email = "nmarks413@gmail.com"; # email (used for certain configurations)
       dotfilesDir = "~/.dotfiles"; # absolute path of the local repo
-      theme = "catpuccin-mocha"; #name of theme that stylix will use
+      theme = "catppuccin-mocha"; #name of theme that stylix will use
       browser = "firefox"; # Default browser; must select one from ./user/app/browser/
       term = "ghostty"; # Default terminal command;
       font = "iosevka"; # Selected font
