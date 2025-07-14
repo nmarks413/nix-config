85cd4c21f2dc8c63c69b38e1f12ae1caefc07520
chloe caruso <git@paperclover.net>
2025-07-14 15:49:59 -0700

    mewow

--- users/chloe/paperback/home.nix
+++ users/chloe/paperback/home.nix
@@ -2,7 +2,6 @@
 {
   # these programs are not globally installed to reduce distractions.
   # most of these are needed for my work environment.
-  programs.bun.enable = true;
   programs.zed-editor.enable = true;
   home.packages = with pkgs; [
     doppler
